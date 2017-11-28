55dd40ef1d842e8ecc3e604c1bd4b66e13265464
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2017-11-28 15:38:14 +0000

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,28 +1,31 @@
 -- a)
 select nome
-from (select nif
-      from ((select nif, categoria from produto)
+from (select nif, categoria
+      from ((select ean, forn_primario as nif, categoria
+             from produto)
             union
-           (select nif, categoria from fornece_sec
-             natural join
-             (select nif, categoria from produto)))
-      group by nif))
-      having count(nif) >= all(select count(nif)
-                               from ((select nif, categoria from produto)
-                                    union
-                                    ((select nif, categoria from fornece_sec)
-                                      natural join
-                                     (select nif, categoria from produto)))
-                              group by nif))
-    natural join fornecedor;
+            (select nif, ean
+             from fornece_sec
+                natural join
+                (select ean, categoria from produto))) as produto_fornecedores
+      group by nif
+      having count(categoria) >= all(select count(categoria)
+                                     from ((select ean, forn_primario as nif, categoria
+                                            from produto)
+                                           union
+                                           (select nif, ean
+                                            from fornece_sec
+                                               natural join
+                                               (select ean, categoria from produto))) as nifs_resposta))
+      natural join fornecedor;
 
 -- b)
 select nif, nome
-from (select nif, nome as categoria
+from (select forn_primario as nif, categoria
       from produto
       group by nif
       having count(distinct categoria) = (select count(nome)
-                                          from categoria_simples))
+                                          from categoria_simples)) as produtos
      natural join
      fornecedor;
 
@@ -41,6 +44,6 @@ having count(nif) > 10;
 -- e)
 select ean
 from(select distinct ean, operador
-     from reposicao)
+     from reposicao) as ean_operador
 group by ean
 having count(operador) = 1;
