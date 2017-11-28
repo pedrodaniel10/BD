5e064d50ccbf6685f28f3525fd7e1eac64fa7634
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2017-11-28 14:43:51 +0000

    sql done

--- queries.sql
+++ queries.sql
@@ -1,5 +1,4 @@
 -- a)
-
 select nome
 from (select nif
       from ((select nif, categoria from produto)
@@ -18,3 +17,30 @@ from (select nif
     natural join fornecedor;
 
 -- b)
+select nif, nome
+from (select nif, nome as categoria
+      from produto
+      group by nif
+      having count(distinct categoria) = (select count(nome)
+                                          from categoria_simples))
+     natural join
+     fornecedor;
+
+-- c)
+select ean
+from produto
+where ean not in (select ean
+                  from reposicao);
+
+-- d)
+select ean
+from fornece_sec
+group by ean
+having count(nif) > 10;
+
+-- e)
+select ean
+from(select distinct ean, operador
+     from reposicao)
+group by ean
+having count(operador) = 1;
