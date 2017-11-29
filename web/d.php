a0ad482f2895a73997ba4f31449f091f82f14b3c
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2017-11-29 00:12:51 +0000

    Search working na d)

--- web/d.php
+++ web/d.php
@@ -2,7 +2,7 @@
   <body>
     <h3>Alterar designa&ccedil;&atilde;o de produtos</h3>
     <form action="d.php" method="post">
-      <p><input type="hidden" name="mode" value="show"/></p>
+      <p><input type="hidden" name="mode" value="search"/></p>
       <p>EAN: <input type="text" name="ean_search"/>
         <input type="submit" value="Procurar"/>
       </p>
@@ -37,6 +37,31 @@
         echo("</table>\n");
         echo("<br><br><a href='index.html'>Voltar</a><br><br>");
     }
+    elseif ($mode == "search"){
+
+      $ean = $_REQUEST['ean_search'];
+
+      $prep = $db->prepare("SELECT ean, design FROM produto WHERE ean = ? ORDER BY ean");
+      $prep->bindParam(1,$ean,PDO::PARAM_INT);
+      $prep->execute();
+      $result = $prep->fetchAll();
+
+      echo("<table border=\"1\" cellspacing=\"5\" style=\"text-align: center\">\n");
+      echo("<tr>\n");
+      echo("<td><b>EAN</b></td>\n");
+      echo("<td><b>Designa&ccedil;&atilde;o</b></td>\n");
+      echo("<td><b>Listar</b></td>\n");
+      echo("</tr>\n");
+        foreach($result as $row){
+            echo("<tr>\n");
+            echo("<td>{$row['ean']}</td>\n");
+            echo("<td>{$row['design']}</td>\n");
+            echo("<td><a href=\"d.php?mode=show&ean={$row['ean']}\">Alterar</a></td>\n");
+            echo("</tr>\n");
+        }
+        echo("</table>\n");
+        echo("<br><br><a href='d.php?mode=home'>Voltar</a><br><br>");
+    }
     elseif ($mode == "show"){
         echo("<h3>EAN = $ean</h3>");
 
@@ -59,7 +84,7 @@
       $result = $prep->fetchAll();
 
       if($result != FALSE){
-        echo("<p>A designa&ccedil;&atilde;o do produto com EAN=$ean para: \"$design\".</p>");
+        echo("<p>A designa&ccedil;&atilde;o do produto com EAN=$ean foi alterada para: \"$design\".</p>");
       }
 
       echo("<br><br><a href='d.php?mode=home'>Voltar</a><br><br>");
