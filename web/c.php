dac70621bda3cbf717a9959dda8d12ba31176f40
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2017-11-29 00:04:43 +0000

    Alterações

--- web/c.php
+++ web/c.php
@@ -2,22 +2,22 @@
   <body>
     <h3>Listar eventos de reposi&ccedil;&atilde;o</h3>
     <form action="c.php" method="post">
-      <p><input type="hidden" name="mode" value="list"/></p>
-      <p>EAN: <input type="text" name="ean"/>
-        <input type="submit" value="Listar"/>
+      <p><input type="hidden" name="mode" value="search"/></p>
+      <p>EAN: <input type="text" name="ean_search"/>
+        <input type="submit" value="Procurar"/>
       </p>
     </form>
 <?php
   try{
-    error_reporting(E_ALL);
-    ini_set('display_errors',1);
+    //error_reporting(E_ALL);
+    //ini_set('display_errors',1);
     include 'config.php';
 
     $mode = isset($_REQUEST['mode']) ? $_REQUEST['mode'] : "";
     $ean = isset($_REQUEST['ean']) ? $_REQUEST['ean'] : "";
     if ($mode == "home"){
 
-      $prep = $db->prepare("SELECT ean, design FROM produto");
+      $prep = $db->prepare("SELECT ean, design FROM produto ORDER BY ean");
       $prep->execute();
       $result = $prep->fetchAll();
 
@@ -31,19 +31,44 @@
             echo("<tr>\n");
             echo("<td>{$row['ean']}</td>\n");
             echo("<td>{$row['design']}</td>\n");
-            echo("<td><a href=\"c.php?mode=list&ean={$row['ean']}\">Listar reposi&ccedil;&otilde;es;</a></td>\n");
+            echo("<td><a href=\"c.php?mode=list&ean={$row['ean']}\">Listar reposi&ccedil;&otilde;es</a></td>\n");
             echo("</tr>\n");
         }
         echo("</table>\n");
         echo("<br><br><a href='index.html'>Voltar</a><br><br>");
+    }
+    elseif ($mode == "search"){
+
+      $ean = $_REQUEST['ean_search'];
+
+      $prep = $db->prepare("SELECT ean, design FROM produto WHERE ean = ? ORDER BY ean");
+      $prep->bindParam(1,$ean,PDO::PARAM_INT);
+      $prep->execute();
+      $result = $prep->fetchAll();
+
+      echo("<table border=\"1\" cellspacing=\"5\" style=\"text-align: center\">\n");
+      echo("<tr>\n");
+      echo("<td><b>EAN</b></td>\n");
+      echo("<td><b>Designa&ccedil;&atilde;o</b></td>\n");
+      echo("<td><b>Listar</b></td>\n");
+      echo("</tr>\n");
+        foreach($result as $row){
+            echo("<tr>\n");
+            echo("<td>{$row['ean']}</td>\n");
+            echo("<td>{$row['design']}</td>\n");
+            echo("<td><a href=\"c.php?mode=list&ean={$row['ean']}\">Listar reposi&ccedil;&otilde;es</a></td>\n");
+            echo("</tr>\n");
+        }
+        echo("</table>\n");
+        echo("<br><br><a href='c.php?mode=home'>Voltar</a><br><br>");
     }
     elseif ($mode == "list"){
 
         echo("<h3>EAN = $ean</h3>");
         $prep = $db->prepare("SELECT operador, instante, unidades
                               FROM evento_reposicao NATURAL JOIN reposicao
-                              WHERE ean = :ean");
-        $prep->bindParam(":ean",$ean);
+                              WHERE ean = ?");
+        $prep->bindParam(1,$ean,PDO::PARAM_INT);
         $prep->execute();
         $result = $prep->fetchAll();
 
